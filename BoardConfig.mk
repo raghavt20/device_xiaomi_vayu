90c97ed25f16dd6785ee72a05ed6a905593c6007
raghavt20 <raghavtandon2000@gmail.com>
2021-09-27 02:09:26 +0530

    vayu: Define screen density

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,5 +8,8 @@ BOARD_VENDOR := xiaomi
 
 DEVICE_PATH := device/xiaomi/vayu
 
+# Screen density
+TARGET_SCREEN_DENSITY := 480
+
 # Inherit from proprietary files
 include vendor/xiaomi/vayu/BoardConfigVendor.mk
