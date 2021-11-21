2ede88477f5f85a9eb7ac1e414ff1222557cc62a
raghavt20 <raghavtandon2000@gmail.com>
2021-11-21 18:20:58 +0000

    vayu: Welcome to WaveOS!

--- wave_vayu.mk
+++ wave_vayu.mk
@@ -11,10 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common WaveOS stuff.
+$(call inherit-product, vendor/wave/configs/common.mk)
 
-PRODUCT_NAME := lineage_vayu
+# Define Boot Animation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := wave_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 Pro
